b77f11bcc8a251e9a01895c7ba2b90c52ac6311c
BornBeBat <Nukkutos@gmail.com>
2024-04-17 16:36:27 +0300

    add card controllers

---
+++ controllers/cardsCtrls/deleteCard.js
@@ -0,0 +1,5 @@
+export const deleteCard = async (req, res, next) => {
+  res.json({
+    ansver: 'deleteCard',
+  });
+};
---
+++ controllers/cardsCtrls/getCardByid.js
@@ -0,0 +1,5 @@
+export const getCardById = async (req, res, next) => {
+  res.json({
+    ansver: 'getCardById',
+  });
+};
