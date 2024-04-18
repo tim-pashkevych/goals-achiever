fe2f8e657822c4b8203038a92d7f640e9dc5e9fe
Kovtunets Mykola <kovtunets73@gmail.com>
2024-04-18 18:32:20 +0200

    auth

--- routes/boardsRouter/boardsRouter.js
+++ routes/boardsRouter/boardsRouter.js
@@ -3,10 +3,11 @@ import { Router } from 'express';
 import validateBody from '../../decorators/validateBody.js';
 import { boardsCtrls } from '../../controllers/index.js';
 import { boardAddSchema, boardUpdateSchema } from '../../schemas/boards/index.js';
+import auth from '../../middlewares/auth.js';
 
 export const boardsRouter = Router();
 
-boardsRouter.post('/', validateBody(boardAddSchema), boardsCtrls.addBoard);
-boardsRouter.get('/:id', boardsCtrls.getBoardById);
-boardsRouter.patch('/:id', validateBody(boardUpdateSchema), boardsCtrls.updateBoard);
-boardsRouter.delete('/:id', boardsCtrls.deleteBoard);
+boardsRouter.post('/', auth, validateBody(boardAddSchema), boardsCtrls.addBoard);
+boardsRouter.get('/:id', auth, boardsCtrls.getBoardById);
+boardsRouter.patch('/:id', auth, validateBody(boardUpdateSchema), boardsCtrls.updateBoard);
+boardsRouter.delete('/:id', auth, boardsCtrls.deleteBoard);
