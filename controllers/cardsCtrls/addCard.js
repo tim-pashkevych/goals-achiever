1a9df9e7486d17c7eddaea4d46280b48649c667b
BornBeBat <Nukkutos@gmail.com>
2024-04-18 21:20:58 +0300

    add cards\delete cards in columns array

--- controllers/cardsCtrls/addCard.js
+++ controllers/cardsCtrls/addCard.js
@@ -3,7 +3,7 @@ import { cardsServices } from '../../services/index.js';
 export const addCard = async (req, res, next) => {
   const { boardId, columnId } = req.params;
 
-  const newCard = await cardsServices.addCard({ ...req.body, boardId, columnId });
+  const newCard = await cardsServices.addCard({ ...req.body, boardId, columnId }, columnId);
 
   res.status(201).json({
     result: newCard,
--- controllers/cardsCtrls/deleteCard.js
+++ controllers/cardsCtrls/deleteCard.js
@@ -2,9 +2,9 @@ import { cardsServices } from '../../services/index.js';
 import HttpError from '../../helpers/HttpError.js';
 
 export const deleteCard = async (req, res, next) => {
-  const { id } = req.params;
+  const { id, columnId } = req.params;
 
-  const result = await cardsServices.removeCard({ _id: id });
+  const result = await cardsServices.removeCard({ _id: id }, columnId);
 
   if (!result) {
     throw HttpError(404);
