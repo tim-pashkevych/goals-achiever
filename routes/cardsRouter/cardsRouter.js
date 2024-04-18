081e709ceac0510cf4b3f05f4b42deb661d9e503
BornBeBat <Nukkutos@gmail.com>
2024-04-18 19:36:44 +0300

    add auth

--- routes/cardsRouter/cardsRouter.js
+++ routes/cardsRouter/cardsRouter.js
@@ -3,13 +3,14 @@ import express from 'express';
 import validateBody from '../../decorators/validateBody.js';
 import { cardAddSchema, cardUpdateSchema } from '../../schemas/cards/index.js';
 import { cardsCtrls } from '../../controllers/index.js';
+import { auth } from '../../middlewares/index.js';
 
 export const cardsRouter = express.Router({ mergeParams: true });
 
-cardsRouter.post('/', validateBody(cardAddSchema), cardsCtrls.addCard);
+cardsRouter.post('/', auth, validateBody(cardAddSchema), cardsCtrls.addCard);
 
-cardsRouter.patch('/:id', validateBody(cardUpdateSchema), cardsCtrls.updateCard);
+cardsRouter.patch('/:id', auth, validateBody(cardUpdateSchema), cardsCtrls.updateCard);
 
-cardsRouter.patch('/:id', cardsCtrls.updateCardColumn);
+cardsRouter.patch('/:id', auth, cardsCtrls.updateCardColumn);
 
-cardsRouter.delete('/:id', cardsCtrls.deleteCard);
+cardsRouter.delete('/:id', auth, cardsCtrls.deleteCard);
