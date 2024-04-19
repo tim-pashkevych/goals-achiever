00ca272aa65d7e0c9f26cb55c73c09e3dc261c80
BornBeBat <Nukkutos@gmail.com>
2024-04-19 12:14:33 +0300

    Upgrade avatar route and base controller

--- controllers/usersControllers/index.js
+++ controllers/usersControllers/index.js
@@ -1,7 +1,9 @@
 import { register } from './register.js';
+import { updateAvatar } from './updateAvatar.js';
 
 import ctrlWrapper from '../../decorators/ctrlWrapper.js';
 
 export const usersControllers = {
   register: ctrlWrapper(register),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
