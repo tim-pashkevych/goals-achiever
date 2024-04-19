82e2961e8bcb8da2e9a12b22273b3e8c367ee761
BornBeBat <Nukkutos@gmail.com>
2024-04-19 15:12:23 +0300

    Swager Cards

--- routes/v1/cardsRouter/cardsRouter.js
+++ routes/v1/cardsRouter/cardsRouter.js
@@ -7,7 +7,7 @@ import { auth } from '../../../middlewares/index.js';
 
 export const cardsRouter = express.Router({ mergeParams: true });
 
-cardsRouter.post('/cards/', auth, validateBody(cardAddSchema), cardsCtrls.addCard);
+cardsRouter.post('/cards', auth, validateBody(cardAddSchema), cardsCtrls.addCard);
 cardsRouter.patch('/cards/:id', auth, validateBody(cardUpdateSchema), cardsCtrls.updateCard);
 cardsRouter.patch('/cards/:id/move', auth, cardsCtrls.moveCard);
 cardsRouter.delete('/cards/:id', auth, cardsCtrls.deleteCard);
--- services/cardsServices/updateCard.js
+++ services/cardsServices/updateCard.js
@@ -3,6 +3,7 @@ import { Card } from '../../models/Card.js';
 export const updateCard = async (filter, data) => {
   const updatedCard = await Card.findOneAndUpdate(filter, data, {
     new: true,
+    fields: ['title', 'description', 'priority', 'deadline'],
   });
   return updatedCard;
 };
