3d688e2948897cdaaf161aaaada5a44ed3284f10
ordila <ordila99@gmail.com>
2024-04-19 18:22:42 +0300

    update deleting ctrls,service

--- controllers/columnsCtrls/deleteColumn.js
+++ controllers/columnsCtrls/deleteColumn.js
@@ -4,9 +4,7 @@ import HttpError from '../../helpers/HttpError.js';
 export const deleteColumn = async (req, res, next) => {
   const { id } = req.params;
 
-  const { boardId } = req.body;
-
-  const result = await columnsServices.removeColumn({ _id: id }, boardId);
+  const result = await columnsServices.removeColumn({ _id: id });
 
   if (!result) {
     throw HttpError(404, 'Column not found');
--- services/columnsServices/removeColumn.js
+++ services/columnsServices/removeColumn.js
@@ -1,8 +1,11 @@
 import { Column } from '../../models/Column.js';
 import { Board } from '../../models/Board.js';
 
-export const removeColumn = async (filter, boardId) => {
+export const removeColumn = async filter => {
   const result = await Column.findOneAndDelete(filter);
+
+  const { boardId } = result;
+
   const id = filter._id;
 
   await Board.findByIdAndUpdate(boardId, { $pull: { columns: { $in: [id] } } }, { new: true });
