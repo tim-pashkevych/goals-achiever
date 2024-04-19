5c3b7b54e72ded9287ac217395b30f177260f7b6
BornBeBat <Nukkutos@gmail.com>
2024-04-19 12:14:02 +0300

    upload middlevare

--- routes/usersRouter/usersRouter.js
+++ routes/usersRouter/usersRouter.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { validateBody } from '../../decorators/index.js';
 import { loginUserSchema } from '../../schemas/index.js';
 import { loginUser, logoutUser } from '../../controllers/index.js';
-import { auth } from '../../middlewares/index.js';
+import { auth, upload } from '../../middlewares/index.js';
 
 import { signupUserSchema } from '../../schemas/users/index.js';
 import { usersControllers } from '../../controllers/usersControllers/index.js';
@@ -15,5 +15,7 @@ usersRouter.post('/register', validateBody(signupUserSchema), usersControllers.r
 usersRouter.post('/login', validateBody(loginUserSchema), loginUser);
 
 usersRouter.post('/logout', auth, logoutUser);
+
+usersRouter.patch('/avatar', upload.single('avatar'), usersControllers.updateAvatar);
 
 export default usersRouter;
