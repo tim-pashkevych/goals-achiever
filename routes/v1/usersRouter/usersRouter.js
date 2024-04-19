478f7bd15ff560dc74ec2ecba03a9a5c8adc5a88
BornBeBat <Nukkutos@gmail.com>
2024-04-19 12:21:23 +0300

    fix new urls

--- routes/v1/usersRouter/usersRouter.js
+++ routes/v1/usersRouter/usersRouter.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { validateBody } from '../../../decorators/index.js';
 import { loginUserSchema } from '../../../schemas/index.js';
 import { loginUser, logoutUser } from '../../../controllers/index.js';
-import { auth } from '../../../middlewares/index.js';
+import { auth, upload } from '../../../middlewares/index.js';
 
 import { signupUserSchema } from '../../../schemas/users/index.js';
 import { usersControllers } from '../../../controllers/usersControllers/index.js';
@@ -13,3 +13,4 @@ export const usersRouter = express.Router();
 usersRouter.post('/users/register', validateBody(signupUserSchema), usersControllers.register);
 usersRouter.post('/users/login', validateBody(loginUserSchema), loginUser);
 usersRouter.post('/users/logout', auth, logoutUser);
+usersRouter.patch('/users/avatar', upload.single('avatar'), usersControllers.updateAvatar);
