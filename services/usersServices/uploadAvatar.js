ff6727400200be81e36ef724f1cd9e45612df028
BornBeBat <Nukkutos@gmail.com>
2024-04-19 17:32:12 +0300

    cloudinaryPublicId

--- services/usersServices/uploadAvatar.js
+++ services/usersServices/uploadAvatar.js
@@ -4,8 +4,8 @@ import { searchUser } from './searchUser.js';
 export const uploadAvatar = async (id, imagePath) => {
   const user = await searchUser({ _id: id });
 
-  if (user.avatarURL) {
-    await cloudinaryServices.removeImage(user.avatarURL);
+  if (user.cloudinaryPublicId) {
+    await cloudinaryServices.removeImage(user.cloudinaryPublicId);
   }
 
   const image = await cloudinaryServices.uploadImage(imagePath, `users/${id}/avatar/`);
