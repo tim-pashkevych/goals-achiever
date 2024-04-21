36e53d52d12e8ac522a69a971f15ef0fb0806ab2
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-04-21 17:08:49 +0300

    Authorization added to the Columns Router

--- controllers/columnsCtrls/updateColumn.js
+++ controllers/columnsCtrls/updateColumn.js
@@ -4,10 +4,6 @@ export const updateColumn = async (req, res, next) => {
   const data = req.body;
   const { id } = req.params;
 
-  // TODO: add a column owner validation
-  // throw new HttpError(404, 'Column not found');
-  // throw new HttpError(401, 'Not Authorized');
-
   const updatedColumn = await columnsServices.updateColumn({ _id: id }, data);
 
   res.json({
