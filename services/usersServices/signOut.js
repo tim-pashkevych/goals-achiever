ebff3067d0a3967f62959a40023ef7ef5d60d6e8
BornBeBat <Nukkutos@gmail.com>
2024-04-30 16:56:26 +0300

    Update signOut.js

--- services/usersServices/signOut.js
+++ services/usersServices/signOut.js
@@ -1,16 +1,6 @@
-import { jwt, passwordSecurity } from '../../helpers/index.js';
 import { User } from '../../models/index.js';
 
 export const signOut = async id => {
-  const user = await User.findById(id);
-
-  if (!user) {
-    const error = new Error('Not authorized');
-    error.status = 401;
-
-    throw error;
-  }
-
   const result = await User.findByIdAndUpdate(id, { token: null, refreshToken: null }, { new: true });
   if (result) {
     return true;
